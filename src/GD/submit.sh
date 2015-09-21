732134f80659b6d6b7f7816b058555c20b54c087
ustcblue <hqz219@gmail.com>
2015-09-21 22:38:26 +0800

    update

--- src/GD/submit.sh
+++
@@ -1,1 +0,0 @@
-spark-submit --master yarn-client --executor-memory 20G --driver-memory 2g --num-executors 2 --executor-cores 4 --py-files ../utils/utils.py train.GD.accumulator.py
--- src/utils/eval_auc.sh
+++
@@ -1,1 +0,0 @@
-sort -n -k $2 $1 | awk -F'\t' '{if($1==0){N=N+1} if($1>0){M=M+1;S=S+NR}}END{print N,M,S,(S-M*(M+1)/2)/(M*N)}'
